567046c2ecef78464e6b5354fe5916240150ed27
Hayder Al-Deen <hayder@etereo.io>
2021-01-22 17:23:29 +0100

    Base components

---
+++ src/components/Conversor/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./conversor.css";
+
+class Conversor extends Component {
+  render() {
+    return (
+      <section className="conversor">
+        <b>Stake</b>
+      </section>
+    );
+  }
+}
+
+export default Conversor;
