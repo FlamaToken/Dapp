fa0a9ab77572e5550cef18505ec072878f42bbef
Hayder Al-Deen <hayder@etereo.io>
2021-01-23 01:02:42 +0100

    Basic layout

--- src/components/Conversor/index.js
+++ src/components/Conversor/index.js
@@ -4,9 +4,39 @@ import "./conversor.css";
 class Conversor extends Component {
   render() {
     return (
-      <section className="conversor">
-        <b>Stake</b>
-      </section>
+      <div className="conversor-wr">
+        <section className="conversor">
+          <div className="option-controller">
+            <a href="#" className="active">
+              Stake
+            </a>
+            <a href="#">Unstake</a>
+          </div>
+          <div className="input-wr">
+            <label for="sendItem">Sent</label>
+            <input id="sendItem" type="number" placeholder="0.0" />
+          </div>
+          <div className="input-wr">
+            <label for="receivedItem">Received</label>
+            <input id="receivedItem" type="number" placeholder="0.0" />
+          </div>
+          <button type="submit" className="conv-btn">
+            Stake
+          </button>
+
+          <div className="wallet-status">
+            <div className="status"></div>
+            <p>Wallet disconnected</p>
+            <a href="#">Connect now</a>
+          </div>
+        </section>
+
+        <small className="note-instructions">
+          1% of FMA transaction goes to Flama Staking Shares (FSS).
+          <br />
+          Stake FMA to get FSS. Unstake FSS to get FMA/FLAPs.
+        </small>
+      </div>
     );
   }
 }
